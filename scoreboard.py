5f16bad1864b35778a33473ac3b54ac614515e47
piksa13 <ania.piktas@gmail.com>
2025-01-01 17:24:06 +0100

    Saving the highest score between games

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.color('white')
         self.penup()
         self.hideturtle()
@@ -19,6 +20,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
